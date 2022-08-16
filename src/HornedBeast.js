82786508cd67d986d73c83bc2ffab40c29c77974
Junyoung Son <junyoungson808@gmail.com>
2022-08-16 16:07:18 -0700

    update lab1

---
+++ src/HornedBeast.js
@@ -0,0 +1,18 @@
+import React from "react";
+import data from './data.json'
+
+class HornedBeast extends React.Component {
+
+    render() {
+
+        return (
+
+            <div className="item">
+            <h2>{this.props.name}</h2>
+            </div>
+
+        )
+    }
+}
+
+export default HornedBeast;
--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import Row from './Row.js';
+import HornedBeast from './HornedBeast.js';
+import data from './data.json';
+
+console.log(data);
 
 class Main extends React.Component {
 
     render() {
         return (
-            <section>
+            <main>
 
-              <Row />
+              <HornedBeast name={data[0].title} />
+              <HornedBeast name={data[1].title} />
+              <HornedBeast name={data[0].title} />
+              <HornedBeast name={data[1].title} />
+              <HornedBeast name={data[0].title} />
+              <HornedBeast name={data[1].title} />
+              <HornedBeast name={data[0].title} />
+              <HornedBeast name={data[1].title} />
 
-            </section>
+            </main>
 
         )
     }
